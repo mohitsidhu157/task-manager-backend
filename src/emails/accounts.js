e587204dde56c54d5e83204bc4261a2f30d47d42
Mohit kumar <mohitsidhu157@gmail.com>
2021-01-02 14:32:10 +0530

    Initial commit

---
+++ src/emails/accounts.js
@@ -0,0 +1,31 @@
+const sgMail = require("@sendgrid/mail")
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (name, email) => {
+	sgMail.send({
+		to: email,
+		from: "mk3051458@yopmail.com",
+		subject: "Welcome to Task Manager",
+		text: `Welcome to the app ${name}. Let me know how you get along with the app.`,
+	
+	}).then(() => {
+		console.log("Email sent.")
+	}).catch(e => {
+		console.log("Error", e )
+	})
+}
+
+const sendCancellationEmail = (name, email) => {
+	sgMail.send({
+		to: email,
+		from: "mk3051458@yopmail.com",
+		subject: "Reason for cancellation",
+		text: `${name}, can you tell us what is the Reason for cancellation of your account?`
+	})
+}
+
+module.exports = {
+	sendWelcomeEmail,
+	sendCancellationEmail
+}
---
+++ src/routers/user.js
@@ -0,0 +1,142 @@
+const express = require('express')
+const multer = require("multer")
+const sharp = require("sharp")
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancellationEmail } = require("../emails/accounts")
+const router = new express.Router()
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        sendWelcomeEmail(user.name, user.email)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        await req.user.remove()
+        sendCancellationEmail(req.user.name, req.user.email)
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+const uploads = multer({
+    limits: {
+        fileSize: 1000000 // 1 MB, in bytes
+    }, 
+    fileFilter(req, file, cb) {
+
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            cb(new Error("Only jpg, jpeg and png image formats are allowed."))
+        }
+
+        cb(undefined, true)
+
+        // cb(new Error("File must be PDF"))
+        // cb(undefined, true) accept file
+        // cb(undefined, false) decline file
+    }
+})
+
+router.post("/user/me/avatar", auth, uploads.single("avatar"), async (req, res) => {
+    try {
+        const buffer = await sharp(req.file.buffer).resize(250, 250).png().toBuffer()
+        req.user.avatar = buffer
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        console.log(e)
+        res.status(500).send()
+    }
+    
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete("/user/me/avatar", auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get("/user/:id/avatar", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set("Content-Type", "image/png")
+
+        res.send(user.avatar)
+    } catch(e) {
+        res.status(404).send()
+    }
+})
+
+module.exports = router
